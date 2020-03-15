99b2d2470710f14e75349a8b659697f4bb84c0aa
MrHaid1 <61915301+MrHaid1@users.noreply.github.com>
2020-03-15 14:41:25 +0800

    add script(hidden left panel)

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let hid = document.querySelector('.main_column_left');
+let btnHidden = document.querySelector('.hidden_button');
+btnHidden.onclick = function () {
+   hid.document.classList.toggle('hidden');
+
+};
