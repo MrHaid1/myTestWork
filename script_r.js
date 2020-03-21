7b9641b34f4a8f5b128803d9264bc8f96b47e1c8
MrHaid1 <61915301+MrHaid1@users.noreply.github.com>
2020-03-22 02:10:10 +0800

    add script_r.js

---
+++ script_r.js
@@ -0,0 +1,6 @@
+var hiColumn = document.querySelector('.main_column_right');
+var btHidden = document.querySelector('.hidden_button_r');
+btHidden.onclick = function () {
+    hiColumn.classList.toggle('hidden')
+
+};
